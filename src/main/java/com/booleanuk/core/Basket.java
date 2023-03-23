765b948dcb754db7af1fc3f65dfbce6a94d6ad2a
Dave Ames <david.john.ames@gmail.com>
2023-03-23 15:03:23 +0000

    Testing if adding a bagel to the basket is successful and stores the bagel name correctly.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,23 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+    ArrayList<String> contents;
+    int capacity;
+    public Basket() {
+        this.contents = new ArrayList<>();
+        this.capacity = 3;
+    }
+
+    public boolean add(String bagel) {
+        this.contents.add(bagel);
+        return true;
+
+    }
+
+    public String toString() {
+        return contents.toString();
+    }
 
 }
