3e86a530b90897f15784a988ae429a4faec5c0b1
Dave Ames <david.john.ames@gmail.com>
2023-03-23 15:09:32 +0000

    Naive solution to adding a bagel means that adding bagels doesn't fail as the capacity isn't taken into account. Test for overfill protection added.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -11,4 +11,15 @@ class BasketTest {
         Assertions.assertEquals("[Plain bagel]", basket.toString());
     }
 
+    @Test
+    public void addBagelToBasketFails() {
+        Basket basket = new Basket();
+        basket.add("Plain bagel");
+        basket.add("Cinnamon and raisin bagel");
+        basket.add("Onion bagel");
+
+        Assertions.assertFalse(basket.add("Sesame bagel"));
+        Assertions.assertEquals("[Plain bagel, Cinnamon and raisin bagel, Onion bagel]", basket.toString());
+    }
+
 }
