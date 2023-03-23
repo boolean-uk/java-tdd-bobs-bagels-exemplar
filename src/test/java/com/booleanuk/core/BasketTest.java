30cf978c9d2e3481f77109936ddb12b3acad9926
Dave Ames <david.john.ames@gmail.com>
2023-03-23 15:02:44 +0000

    Testing if adding a bagel to the basket is successful and stores the bagel name correctly.

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
+    @Test
+    public void addBagelToBasketSucceeds() {
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.add("Plain bagel"));
+        Assertions.assertEquals("[Plain bagel]", basket.toString());
+    }
 
 }
